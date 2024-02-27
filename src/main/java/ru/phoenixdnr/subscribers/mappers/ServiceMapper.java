999520db93c2a5eeb0560c49fff48e393c619ff1
ilya34610 <zohtud18@gmail.com>
2024-02-27 23:20:04 +0300

    feat: добавление маппинга для сущностей

---
+++ src/main/java/ru/phoenixdnr/subscribers/mappers/ServiceMapper.java
@@ -0,0 +1,14 @@
+package ru.phoenixdnr.subscribers.mappers;
+
+import org.springframework.stereotype.Service;
+import ru.phoenixdnr.subscribers.dto.input.ServiceInput;
+import ru.phoenixdnr.subscribers.entity.ServiceEntity;
+
+@Service
+public class ServiceMapper {
+    public ServiceEntity fromInput(ServiceInput serviceInput, ServiceEntity entity) {
+        entity.setName(serviceInput.getName());
+        entity.setCost(serviceInput.getCost());
+        return entity;
+    }
+}
